87f5e50a415651d55f32c792dd5e1ce7498a8296
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-08-02 18:54:22 -0300

    exercicios parte for-in-ofa

--- 01-fundamentos/bloco-04-intro-javascript-logProgramacao/dia-4-javascript-objetos-funcoes/for-in-ex.js
+++ 01-fundamentos/bloco-04-intro-javascript-logProgramacao/dia-4-javascript-objetos-funcoes/for-in-ex.js
@@ -1,26 +1,65 @@
 // for/in - madeira mais dinamica de criar um for.
 /* muito utilizado em objetos */
 
-let cars = ['Saab', 'Volvo', 'BMW'];
+// mostrando as marcar dos carros (valores);
+// let cars = ['Saab', 'Volvo', 'BMW'];
 
-for (let index in cars) {
-  console.log(cars[index]); // assim eu mostro chaves e valores. o [index] e o que mostra os valores, e o nome do objeto;
-}
+// for (let index in cars) {
+//   console.log(cars[index]); // assim eu mostro chaves e valores. o [index] e o que mostra os valores, e o nome do objeto;
+// }
 
 // Um outro exemplo é a iteração nas chaves de um objeto:
 
-let car = {
-  type: 'Fiat',
-  model: '500',
-  color: 'white',
+// let car = {
+//   type: 'Fiat',
+//   model: '500',
+//   color: 'white',
+// };
+
+// for (let index in car) {
+//   console.log(index, car[index]);
+// }
+
+
+// diferença for/in e for/of - O For/in irá percorrer a propriedade declarada, e não o seu valor em si. - o For/of percorre os elementos dos objetos iteráveis através dos seus respectivos valores, e não dos índices como o For/in .
+// let food = ['hamburguer', 'bife', 'acarajé'];
+// for (let position in food) {
+//   console.log(position);
+// };
+//resultado: 0, 1, 2;
+
+// let food = ['hamburguer', 'bife', 'acarajé'];
+// for (let value of food) {
+//   console.log(value);
+// };
+//resultado: hamburguer, bife, acarajé;
+
+
+//PRATICANDO
+// Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
 };
 
-for (let index in car) {
-  console.log(index, car[index]);
+ for (let value in names) {
+   console.log('Olá, ' + value + ': ' + names[value]);
+ }
+
+// Usando o objeto abaixo, utilize For/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let index of car) {
+  console.log(index + ':  ' + car[index]);
 }
 
 
-for (let key in car) {
-  console.log(key);
-}
+
 
