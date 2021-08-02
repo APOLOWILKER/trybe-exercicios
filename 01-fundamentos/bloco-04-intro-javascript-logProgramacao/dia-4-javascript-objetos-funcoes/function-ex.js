49badd4fe7d6569ad2bda48c81ef6ba255a35689
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-08-02 17:34:25 -0300

    estudo do function

---
+++ 01-fundamentos/bloco-04-intro-javascript-logProgramacao/dia-4-javascript-objetos-funcoes/function-ex.js
@@ -0,0 +1,22 @@
+// Função seria um agrupador de código. As funções são reutilizaveis.
+
+// function greet(name, age) {
+//   console.log('Ola, ' + name + ' . Você possui  ' + age + ' anos.');
+// }
+
+// greet( 'Apolo', 28);
+
+// function makeGreet(name, age) {
+//   return 'Ola, ' + name + ' . Você possui  ' + age + ' anos.';
+// }
+
+// console.log(makeGreet('Apolo', 28));
+
+let number1 = 30;
+let number2 = 20;
+
+console.log(number1 +  number2);
+function sum(number1, number2){
+  return number1 + number2;
+}
+console.log(sum(60,25))
