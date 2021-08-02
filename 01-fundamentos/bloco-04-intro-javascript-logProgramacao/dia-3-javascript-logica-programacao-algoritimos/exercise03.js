280842213d2f5b4458daad240e032e7c536815eb
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-08-02 13:02:16 -0300

    exercicios 1 2 e 3 concluidos atualizacao

--- 01-fundamentos/bloco-04-intro-javascript-logProgramacao/dia-3-javascript-logica-programacao-algoritimos/exercise03.js
+++ 01-fundamentos/bloco-04-intro-javascript-logProgramacao/dia-3-javascript-logica-programacao-algoritimos/exercise03.js
@@ -1,15 +1,22 @@
 // Agora inverta o lado do triângulo. 
 
-let n = 5
-let asterisco = " * ";
-let recebeSimbolo = "";
-let espaco = " - "
+let n = 5;
+let asterisco = "*";
+let linhaEntrada = "";
+let posicaoEntrada = n;
 
 
-for (index = 0; index <= n; index += 1){   
-  console.log(recebeSimbolo);
-  recebeSimbolo += asterisco;  /*ou recebeSimbolo += asterisco */
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1){
+  for (let columnIndex = 0;columnIndex <= n; columnIndex += 1) {
+    if(columnIndex < posicaoEntrada) {
+      linhaEntrada += ' ';
+    } else {
+      linhaEntrada +=  asterisco;
+    }
+  }   
+  console.log(linhaEntrada);
+  linhaEntrada = '';
+  posicaoEntrada -= 1;
 }
-
 
 
