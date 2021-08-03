af8b0746ad7b726b16724e97f957e40383c9b44a
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-08-02 21:23:47 -0300

    exercicios parte 1 numero4

---
+++ 01-fundamentos/bloco-04-intro-javascript-logProgramacao/dia-4-javascript-objetos-funcoes/exercises/exercise04.js
@@ -0,0 +1,12 @@
+// 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+};
+
+for(let index in info){
+  console.log(info[index]);
+}
