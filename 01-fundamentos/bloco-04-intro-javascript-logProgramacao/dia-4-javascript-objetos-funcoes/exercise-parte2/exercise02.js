675e13436024e8c863116f06e3879e3a59789786
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-08-02 23:14:09 -0300

    exercicios parte 2 numero 2

---
+++ 01-fundamentos/bloco-04-intro-javascript-logProgramacao/dia-4-javascript-objetos-funcoes/exercise-parte2/exercise02.js
@@ -0,0 +1,16 @@
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+let arrayInteiro = [2, 6, 7, 10, 1];
+
+function maiorValor (array) {
+  let indiceMaior = 1;
+  for (let index in array) {
+    if(array[indiceMaior] < array[index]){
+      indiceMaior = index;
+    }
+  }
+  return indiceMaior;
+}
+
+console.log(maiorValor(arrayInteiro));
+
