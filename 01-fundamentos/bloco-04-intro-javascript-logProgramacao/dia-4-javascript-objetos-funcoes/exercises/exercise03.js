e654f0c5bee19328192dafb957541064a1f58d73
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-08-02 21:18:50 -0300

    exercicios parte 1 numero 3

--- 01-fundamentos/bloco-04-intro-javascript-logProgramacao/dia-4-javascript-objetos-funcoes/exercises/exercise03.js
+++ 01-fundamentos/bloco-04-intro-javascript-logProgramacao/dia-4-javascript-objetos-funcoes/exercises/exercise03.js
@@ -4,4 +4,13 @@
 // nota
 // recorrente
 
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+};
 
+for (let key in info) {
+  console.log(key);
+};
