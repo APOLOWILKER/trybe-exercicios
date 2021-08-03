320b837aa9df7b4e40f0ddb57895cf84227063d3
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-08-03 11:34:45 -0300

    exercicios parte 2 refatorando

--- 01-fundamentos/bloco-04-intro-javascript-logProgramacao/dia-4-javascript-objetos-funcoes/exercise-parte2/exercise04.js
+++ 01-fundamentos/bloco-04-intro-javascript-logProgramacao/dia-4-javascript-objetos-funcoes/exercise-parte2/exercise04.js
@@ -1,15 +1,15 @@
 //4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 
-let teste = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; 
+let teste = ['José', 'Lucas', 'Nádia', 'Fernandos', 'Cairo', 'Joana', 'Fernandas', 'zico', 'Zico', 'álvo']; 
 
-function maisCaracteres (string) {
-  let maiorString = 0;
-  for (let index in string) {
-    if(string[maiorString] > string[index]){
-      maiorString = teste[index];
+function maisCaracteres (listName) {
+  let indiceMaiorString = 0;
+  for (let index in listName) {
+    if(listName[indiceMaiorString] <= listName[index]) {
+      indiceMaiorString = index;
     }
   }
-  return maiorString;
+  return listName[indiceMaiorString];
 }
 
 console.log(maisCaracteres(teste));
--- 01-fundamentos/bloco-04-intro-javascript-logProgramacao/dia-4-javascript-objetos-funcoes/exercise-parte2/exercise05.js
+++ 01-fundamentos/bloco-04-intro-javascript-logProgramacao/dia-4-javascript-objetos-funcoes/exercise-parte2/exercise05.js
@@ -1,26 +1,26 @@
 // 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 
 
-let teste = [2, 3, 2, 5, 8, 2, 3, 3, 3, 3];
+let teste = [2, 3, 2, 5, 8, 2, 3, 3, 3, 3, 3];
 
 function repeteInteiro (inteiro) {
   let contRepetido = 0;
   let repeat = 0;
-  let indexNumeroRepetido = 0;
+  let NumeroRepetido = 0;
   for (let index in inteiro) {
     let verificaNumero = inteiro[index]
-  for (let index2 in inteiro) {
-    if (verificaNumero === inteiro[index2]) {
-      repeat += 1;
+    for (let index2 in inteiro) {
+      if (verificaNumero === inteiro[index2]) {
+        repeat += 1;
+      }
     }
-  }
-  if (repeat > contRepetido) {
-    contRepetido = repeat;
-    indexNumeroRepetido = index;
-  }
-  repeat = 0;
+    if (repeat > contRepetido) {
+      contRepetido = repeat;
+      NumeroRepetido = inteiro[index];
+    }
+    repeat = 0;
 }
-  return inteiro[indexNumeroRepetido];
+  return NumeroRepetido;
 }
 
 console.log(repeteInteiro(teste));
