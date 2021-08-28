b268b0ada50d72102cecf68fe9715e464fb44cd8
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-08-27 23:02:29 -0300

    exercicio 1 parte 2

---
+++ 01-fundamentos/bloco-07-intro-javaScriptES6-testeUni/dia-1-let-const-arrowFunction-templateLiterals/Parte2-exercicio1/part2-script.js
@@ -0,0 +1,7 @@
+/* Crie uma função que receba um número e retorne seu fatorial.
+Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator . */
+
+const fatorial = (n) => n > 1 ? n * fatorial(n - 1) : 1;
+
+console.log(fatorial(4)); //pego pela matemática aqui !
